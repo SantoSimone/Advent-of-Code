e39525c5eef66300f773b8cbd941fd5de5f16dbd
Simone <simone.santillo@deepvisionconsulting.com>
2022-12-05 18:13:49 +0100

    Day 5

---
+++ 2022/day_5.py
@@ -0,0 +1,41 @@
+import re
+from collections import defaultdict
+from typing import List
+
+import python_utils
+
+
+def d5p1(lines: List[str]):
+    stacks = defaultdict(list)
+    init_lines, commands = python_utils.splitter(lines, "")
+    for line in reversed(init_lines[:-1]):
+        for i, c in enumerate(line[1::4]):
+            if c != " ":
+                stacks[i + 1].append(c)
+    for cmd in commands:
+        num, src, dst = map(int, re.findall(r'\d+', cmd))
+        to_move = [stacks[src].pop() for _ in range(num)]
+        stacks[dst].extend(to_move)
+        
+    return [s[-1:] for s in stacks.values()]
+
+
+def d5p2(lines: List[str]):
+    stacks = defaultdict(list)
+    init_lines, commands = python_utils.splitter(lines, "")
+    for line in reversed(init_lines[:-1]):
+        for i, c in enumerate(line[1::4]):
+            if c != " ":
+                stacks[i + 1].append(c)
+    for cmd in commands:
+        num, src, dst = map(int, re.findall(r'\d+', cmd))
+        to_move = [stacks[src].pop() for _ in range(num)]
+        stacks[dst].extend(reversed(to_move))
+
+    return [s[-1:] for s in stacks.values()]
+
+
+if __name__ == '__main__':
+    lines = python_utils.get_input_as_lines(5, 2022)
+    print(f"Part 1: {d5p1(lines)}")
+    print(f"Part 2: {d5p2(lines)}")
