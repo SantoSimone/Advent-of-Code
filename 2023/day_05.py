e0884a5684b460f0be98d23c35d9bc85498ede4c
Simone Santillo <simone.santillo@gmail.com>
2023-12-07 12:06:59 +0100

    Way better day 5

--- 2023/day_05.py
+++ 2023/day_05.py
@@ -1,5 +1,4 @@
 import re
-import time
 from typing import List, Tuple
 
 import more_itertools
@@ -21,7 +20,7 @@ def parse_ranges(lines: List[str]) -> List[Tuple[int, int, int]]:
 
 
 def d5p1(lines: List[str]):
-    seeds = map(int, re.findall(r'\d+', lines[0]))
+    seeds = python_utils.parse_ints(lines[0])
     grouped_ranges = parse_ranges(lines)
 
     locations = []
@@ -38,60 +37,45 @@ def d5p1(lines: List[str]):
 
 
 def d5p2(lines: List[str]):
-    seeds = map(int, re.findall(r'\d+', lines[0]))
+    # Code is a bit convoluted, but the idea is to perform mapping each seed chunk at a time instead of single seed
+    # Btw, brute force took 2h 15min... this solution 5 ms :)
+    seeds = python_utils.parse_ints(lines[0])
     grouped_ranges = parse_ranges(lines)
 
-    locations = []
-    for seed_start, seed_length in more_itertools.chunked(seeds, 2):
-        for seed in range(seed_start, seed_start + seed_length):
-            for group in grouped_ranges:
-                for dst_start, src_start, length in group:
-                    if src_start <= seed < src_start + length:
-                        seed = dst_start + (seed - src_start)
-                        break
+    chunk_to_check = [(start, start + length) for start, length in more_itertools.chunked(seeds, 2)]
+    for group in grouped_ranges:
+        new_chunks = []
+        while chunk_to_check:
+            curr_start, curr_end = chunk_to_check.pop()
+            matched = []
+            for dst_start, src_start, length in group:
+                intersection_start = max(curr_start, src_start)
+                intersection_end = min(curr_end, src_start + length)
+                if intersection_start < intersection_end - 1:
+                    new_chunks.append(
+                        (dst_start + (intersection_start - src_start),
+                         dst_start + (intersection_start - src_start) + (intersection_end - intersection_start))
+                    )
+                    matched.append((intersection_start, intersection_end))
+            else:
+                # Check that the current chunk has been fully covered
+                fill_start = curr_start
+                matched = sorted(matched)
+                for start, end in matched:
+                    if start > fill_start:
+                        new_chunks.append((fill_start, start))
+                    fill_start = end + 1
+                else:
+                    if curr_end > fill_start:
+                        new_chunks.append((fill_start, curr_end))
 
-            locations.append(seed)
+        chunk_to_check = new_chunks
 
-    return min(locations)
+    return min(chunk_min for chunk_min, _ in chunk_to_check)
 
 
 if __name__ == '__main__':
     day, year = 5, 2023
     lines = python_utils.get_input_as_lines(day, year)
-    #     lines = """seeds: 79 14 55 13
-    #
-    # seed-to-soil map:
-    # 50 98 2
-    # 52 50 48
-    #
-    # soil-to-fertilizer map:
-    # 0 15 37
-    # 37 52 2
-    # 39 0 15
-    #
-    # fertilizer-to-water map:
-    # 49 53 8
-    # 0 11 42
-    # 42 0 7
-    # 57 7 4
-    #
-    # water-to-light map:
-    # 88 18 7
-    # 18 25 70
-    #
-    # light-to-temperature map:
-    # 45 77 23
-    # 81 45 19
-    # 68 64 13
-    #
-    # temperature-to-humidity map:
-    # 0 69 1
-    # 1 0 69
-    #
-    # humidity-to-location map:
-    # 60 56 37
-    # 56 93 4""".splitlines()
     print(f'Part 1: {d5p1(lines)}')
-    start = time.perf_counter()
     print(f'Part 2: {d5p2(lines)}')
-    print(f"Total time: {time.perf_counter() - start}")
